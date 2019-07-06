6ac45e1d7adbcb09c438ed5b234ae58c06873b9d
balkanskiy <balkanskiy.alexey@gmail.com>
2019-07-06 22:09:24 +0300

    Add mail(), resolve koan with Safe Calls

--- src/app.kt
+++ src/app.kt
@@ -1,29 +1,20 @@
-class JavaCode {
-    fun toJSON(collection: Collection<Int>): String {
-        val sb = StringBuilder()
-        sb.append("[")
-        val iterator = collection.iterator()
-        while (iterator.hasNext()) {
-            val element = iterator.next()
-            sb.append(element)
-            if (iterator.hasNext()) {
-                sb.append(", ")
-            }
+fun main(args: Array<String>) {
+    fun sendMessageToClient(client: Client?, message: String?, mailer: Mailer) {
+        if (client == null || message == null) return
+        var email = client?.personalInfo?.email
+
+        email?.let{
+            mailer.sendMessage(email, message)
         }
-        sb.append("]")
-        return sb.toString()
+
     }
 }
 
-fun containsEven(collection: Collection<Int>) = collection.any { it % 2 === 0 }
-fun containsEvenWithIt(collection: Collection<Int>) = collection.any { i -> i % 2 === 0 }
-
-val month = "(JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)"
-fun getPattern(): String = """\d{2} $month \d{4}""";
-
-// investigate data annotation for classes
-data class Person(val name: String, val age: Int)
-
-fun getPeople(): List<Person> {
-    return listOf(Person("Alice", 29), Person("Bob", 31))
+class Client (val personalInfo: PersonalInfo?)
+class PersonalInfo (val email: String?)
+interface Mailer {
+    fun sendMessage(email: String, message: String)
 }
+
+
+
---
+++ src/koans.kt
@@ -0,0 +1,29 @@
+class JavaCode {
+    fun toJSON(collection: Collection<Int>): String {
+        val sb = StringBuilder()
+        sb.append("[")
+        val iterator = collection.iterator()
+        while (iterator.hasNext()) {
+            val element = iterator.next()
+            sb.append(element)
+            if (iterator.hasNext()) {
+                sb.append(", ")
+            }
+        }
+        sb.append("]")
+        return sb.toString()
+    }
+}
+
+fun containsEven(collection: Collection<Int>) = collection.any { it % 2 === 0 }
+fun containsEvenWithIt(collection: Collection<Int>) = collection.any { i -> i % 2 === 0 }
+
+val month = "(JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)"
+fun getPattern(): String = """\d{2} $month \d{4}""";
+
+// investigate data annotation for classes
+data class Person(val name: String, val age: Int)
+
+fun getPeople(): List<Person> {
+    return listOf(Person("Alice", 29), Person("Bob", 31))
+}
